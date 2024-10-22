6b4b248955a67ee3ece56450fe26fd1f306669d0
Matthew James <69026158+Matt-DESTROYER@users.noreply.github.com>
2024-10-22 17:22:23 +1100

    Modify car handling

--- src/Self-Driving-Car/Modules/Car.js
+++ src/Self-Driving-Car/Modules/Car.js
@@ -8,8 +8,8 @@ class Car {
 		this.height = height;
 
 		this.speed = 0;
-		this.acceleration = 0.3;
-		this.maxSpeed = 8;
+		this.acceleration = 0.2;
+		this.maxSpeed = 3;
 		this.friction = 0.05;
 		this.rotationSpeed = 0.04;
 		this.rotation = 0;
